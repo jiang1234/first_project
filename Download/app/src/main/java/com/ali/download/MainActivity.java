ca641465d973132eb7c983bfd08c37fe49141e57
jiang1234 <1246272441@qq.com>
2017-07-26 21:39:07 +0800

    Add a new test activity.

---
+++ Download/app/src/main/java/com/ali/download/MainActivity.java
@@ -0,0 +1,10 @@
+package com.ali.download;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017/7/26.
+ */
+
+public class MainActivity extends AppCompatActivity {
+}
